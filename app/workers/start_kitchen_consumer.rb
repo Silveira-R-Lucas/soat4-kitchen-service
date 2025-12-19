6dc4689b6b009646fc888afd1b053d123089445b
Lucas Silveira <silveira.codes@gmail.com>
2025-12-18 23:30:41 -0300

    adjust consumer worker

--- app/workers/start_kitchen_consumer.rb
+++ app/workers/start_kitchen_consumer.rb
@@ -1,1 +1,7 @@
-RabbitmqConsumer.new("pagamento.events", "kitchen.pagamento-aprovado").start_listening
+# frozen_string_literal: true
+
+class StartKitchenConsumer
+  def self.run
+    RabbitmqConsumer.new("pagamento.events", "kitchen.pagamento-aprovado").start_listening
+  end
+end
